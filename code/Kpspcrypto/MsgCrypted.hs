2ff15dbde2158f27da19b973db6693300a23cbf3
Pascal Schwarz <iso@thinktux.net>
2013-05-25 21:31:18 +0200

    msgcrypted angefangen

--- code/Kpspcrypto/MsgCrypted.hs
+++ code/Kpspcrypto/MsgCrypted.hs
@@ -6,3 +6,14 @@ module Kpspcrypto.MsgCrypted where
 
 import qualified Data.ByteString.Char8 as B
 import System.Random
+
+import Kpspcrypto.Msg
+
+type Key = B.ByteString
+type SymCipher = B.ByteString
+type ChainMode = B.ByteString
+
+genMsgPart :: StdGen -> SymCipher -> ChainMode -> B.ByteString -> (MsgPart, Key)
+genMsgPart rgen cipher mode plain = (MsgPart MSGCRYPTED [cipher,mode] plain, "ourkey")
+
+-- was in diesem sinne, rekursiv: randomR (0,255) (mkStdGen 12314) :: (Int, StdGen)
