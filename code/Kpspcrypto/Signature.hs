7fc41c9c9f7035e3f88fb4055f74a2d2b4054f5b
Pascal Schwarz <iso@thinktux.net>
2013-05-28 18:12:18 +0200

    verifySig ... puh

--- code/Kpspcrypto/Signature.hs
+++ code/Kpspcrypto/Signature.hs
@@ -5,7 +5,9 @@ module Kpspcrypto.Signature where
 {-# LANGUAGE OverloadedStrings #-}
 
 import qualified Data.ByteString.Char8 as B
+import qualified Data.Map as M
 import Data.List
+import Data.Maybe
 
 import qualified Kpspcrypto.Base64 as B64
 import qualified Kpspcrypto.RSA as RSA
@@ -20,12 +22,24 @@ genMsgPart "RSA" akey "SHA256" [kcpart,msgcpart] = MsgPart SIGNATURE ["RSA","SHA
 		signed = map B64.encode [RSA.sign akey block | block <- pad 1 hashed] --TODO: groessere Keys, dann auf 8 Byte oder so anwenden
 		signature = B.intercalate "," signed
 
-verifySig :: [MsgPart] -> Bool
-verifySig parts = True 
+verifySig :: AsymKey -> [MsgPart] -> Bool
+verifySig akey parts = and $ zipWith (checksig akey) bsigs bs
 	where
 		[kpart,mpart,spart] = sort parts
 		[k,m,s] = map content [kpart,mpart,spart]
-		msg = k `B.append` m
+		msgh = hashf k `B.append` m
+		bsigs = [B64.decode block | block <- B.split ',' s]
+		bs = pad 1 msgh
+		sigtype = options spart !! 0
+		hashtype = options spart !! 1
+		checksig = fromJust $ M.lookup sigtype checksigs
+		hashf = fromJust $ M.lookup hashtype hashfs
+
+hashfs :: M.Map HashType (B.ByteString -> B.ByteString)
+hashfs = M.fromList [("SHA256",SHA.hash)]
+
+checksigs :: M.Map AsymCipher (AsymKey -> B.ByteString -> B.ByteString -> Bool)
+checksigs = M.fromList [("RSA",RSA.checksig)]
 
 rsapubkey = "----BEGIN RSA PUBLIC KEY----\nFw==,jw==\n----END RSA PUBLIC KEY----" :: B.ByteString
 rsaprivkey = "----BEGIN RSA PRIVATE KEY----\nLw==,jw==\n----END RSA PRIVATE KEY----" :: B.ByteString
