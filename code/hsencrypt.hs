16001b9449f36155ab90341403078e6df91ba8e9
sam <sam@sam.ch>
2013-06-02 23:32:57 +0200

    fdvgs

--- code/hsencrypt.hs
+++ code/hsencrypt.hs
@@ -5,13 +5,19 @@
 -- runhaskell -XOverloadedStrings hsencrypt.hs params...
 
 import System.Environment
+import System.Random
+import qualified Data.ByteString.Char8 as B
 import Kpspcrypto.Msg
+import qualified Kpspcrypto.MsgCrypted as M
+import qualified Kpspcrypto.KeyCrypted as K
+import qualified Kpspcrypto.Signature as S
 
 main = do
 	args <- getArgs
-	handleArgs args
-
-handleArgs :: [String] -> IO()
+	handleArgs $ map B.pack args
+		
+	
+handleArgs :: [B.ByteString] -> IO()
 handleArgs args = do	
 	if length args /= 6 then do
 		printUsage
@@ -20,23 +26,25 @@ handleArgs args = do
 		let hash = args !! 1
 		let sym = args !! 2
 		let blockmode = args !! 3
-		let rcptpubkey = args !! 4
-		let infile = args !! 5
-		putStr "Asym Cipher: "
-		putStrLn asym
-		putStr "Hash Alg: "
-		putStrLn hash
-		putStr "Sym Cipher: "
-		putStrLn sym
-		putStr "Block Chaining Mode: "
-		putStrLn blockmode
-		putStr "Public Key of Recipient: "
-		putStrLn rcptpubkey
-		putStr "File with Plaintext: "
-		putStrLn infile
+		let ownprivkey = args !! 4
+		let rcptpubkey = args !! 5
+		let infile = args !! 6
+		pubkey <- B.readFile $ B.unpack rcptpubkey
+		privkey <- B.readFile $ B.unpack ownprivkey
+		plainFileContent <- B.readFile $ B.unpack infile
+		rgen <- getStdGen
+		let (mMsgPart, symkey) = M.genMsgPart rgen sym blockmode plainFileContent  --generates the crypted Message
+		let kMsgPart = K.genMsgPart asym pubkey symkey  --generates the crypted Key
+		let plainS = [kMsgPart,mMsgPart]
+		let sMsgPart = S.genMsgPart asym privkey hash $ plainS  --generates the signature
+		let msgParts = map (B.pack . show) [kMsgPart,mMsgPart,sMsgPart]
+		B.writeFile (B.unpack infile ++ "Encrypted") $ B.intercalate "\n\n" msgParts
 
+		
 printUsage :: IO()
 printUsage = do
 	putStrLn "you need to call this binary in this way:"
-	putStrLn "hsencrypt asymCipher hashAlg symCipher chainingMode publicKey plainFile"
-	putStrLn "Example: hsencrypt RSA SHA256 AES256 CBC pubkey plaintext.txt"
+	putStrLn "hsencrypt asymCipher hashAlg symCipher chainingMode privKey publicKey plainFile"
+	putStrLn "Example: hsencrypt RSA SHA256 AES256 CBC privkey pubkey plaintext.txt"
+	
+
