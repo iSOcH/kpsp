19544a03b23834ee66bbc9c96750282ee1add195
Pascal Schwarz <iso@thinktux.net>
2013-06-02 22:12:00 +0200

    comments

--- code/Kpspcrypto/Serial.hs
+++ code/Kpspcrypto/Serial.hs
@@ -7,16 +7,20 @@ module Kpspcrypto.Serial where
 import qualified Data.ByteString.Char8 as B
 import Data.Char
 
+-- interpret the bytes of a ByteString as
+-- Integer, result will always be positive
 asInt :: B.ByteString -> Integer
 asInt str = f . reverse $ B.unpack str
 	where
 		f "" = 0
-		f (x:xs) = toInteger(ord x) + 256 * f xs
+		f (x:xs) = toInteger (ord x) + 256 * f xs
 
+-- convert a positive Integer to a ByteString
 asStr :: Integer -> B.ByteString
+-- edge case here because we dont want 0 to become "", but
+-- we need a ""-edge-case in the helper function
 asStr 0 = B.singleton '\0'
 asStr i = B.pack . reverse $ f i
 	where
-		f :: Integer -> [Char]
 		f 0 = []
 		f i = chr (fromInteger (i `mod` 256)) : f (i `div` 256)
