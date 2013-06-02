ff8d4eaca271b4aeaa795a1e8fe8fedd137edffc
Pascal Schwarz <iso@thinktux.net>
2013-06-02 10:28:42 +0200

    comments on data, some tests

--- code/Kpspcrypto/SHA256.hs
+++ code/Kpspcrypto/SHA256.hs
@@ -10,6 +10,7 @@ import qualified Data.ByteString.Char8 as B
 import Data.Word
 import Data.Bits
 import Data.Char
+import Text.Printf -- for tests only
 
 import Kpspcrypto.Pad
 import Kpspcrypto.Serial
@@ -95,10 +96,13 @@ chunks = block 64
 {---
 Data
 ---}
+-- initial hash
 hs :: [Word32]
 hs =
 	[0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19]
 
+-- round constants, in every iteration of the innermost
+-- loop (mainloop), one of these values is used
 ks :: [Word32]
 ks = 
 	[0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5
@@ -110,3 +114,38 @@ ks =
 	,0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3
 	,0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
 	]
+
+{---------------------------------------
+some tests
+data from wikipedia and manual execution
+of echo -ne "input" | sha256sum on linux
+---------------------------------------}
+
+-- printf "%08x" wandelt einen Int in die Hexdarstellung um
+-- üblicherweise werden hashes in Hex ausgegeben
+hex :: B.ByteString -> B.ByteString
+hex = B.pack . printf "%08x" . asInt
+
+-- true if no tests failed
+runtests :: Bool
+runtests = and [testHash test | test <- tests]
+
+testHash :: (B.ByteString,B.ByteString) -> Bool
+testHash (input,expected) = (hex $ hash input) == expected
+
+tests = [("","e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
+		,("\0","6e340b9cffb37a989ca544e6bb780a2c78901d3fb33738768511a30617afa01d")
+		,("a","ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb")
+		,("hallo","d3751d33f9cd5049c4af2b462735457e4d3baf130bcbb87f389e349fbaeb20b9")
+		-- 55 bytes stay in one chunk
+		,(B.replicate 55 'a',"9f4390f8d30c2dd92ec9f095b65e2b9ae9b0a925a5258e241c9f1e910f734318")
+		-- 56 bytes and more cause perchunk to be called at least two times
+		,(B.replicate 56 'a',"b35439a4ac6f0948b6d6f9e3c6af0f5f590ce20f1bde7090ef7970686ec6738a")
+		,(B.replicate 57 'b',"2dd0a6d14520f410e18bd2f443f0ff2e7389dfaf9242bb9257730fc190e8085d")
+		,("Franz jagt im komplett verwahrlosten Taxi quer durch Bayern",
+			"d32b568cd1b96d459e7291ebf4b25d007f275c9f13149beeb782fac0716613f8")
+		,("Frank jagt im komplett verwahrlosten Taxi quer durch Bayern",
+			"78206a866dbb2bf017d8e34274aed01a8ce405b69d45db30bafa00f5eeed7d5e")
+		,(B.replicate 120 'a' `B.append` B.replicate 1000 'Z',
+			"f44da844b446469e8a3c928e6f696b3994e404b1388282267e932744bbc74c34")
+		]
