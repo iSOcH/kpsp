77a573862af62796102ab6c37b8573a2405643c5
Pascal Schwarz <iso@thinktux.net>
2013-06-02 21:46:31 +0200

    cbc scheint korrekt, denn plain == (uncbc d iv $ cbc e iv plain) gibt True

--- code/Kpspcrypto/BlockModes.hs
+++ code/Kpspcrypto/BlockModes.hs
@@ -5,17 +5,40 @@ module Kpspcrypto.BlockModes where
 {-# LANGUAGE OverloadedStrings #-}
 
 import qualified Data.ByteString.Char8 as B
+import Data.Bits
 
 import Kpspcrypto.Pad
+import Kpspcrypto.Serial
 
 type Block = B.ByteString
 type IV = Block
 
 cbc :: (Block -> Block) -> IV -> B.ByteString -> B.ByteString
-cbc cipher iv plain = id plain
+cbc cipher iv plain = B.concat $ docbc cipher iv $ block blocklen plain
+	where
+		blocklen = B.length iv
+
+docbc :: (Block -> Block) -> IV -> [Block] -> [Block]
+docbc _ _ [] = []
+docbc cipher iv (x:xs) = cblock : docbc cipher cblock xs
+	where
+		ivi = asInt iv
+		xi = asInt x
+		ivxorb = asStr $ ivi `xor` xi
+		cblock = cipher ivxorb
 
 uncbc :: (Block -> Block) -> IV -> B.ByteString -> B.ByteString
-uncbc cipher iv crypt = id crypt
+uncbc cipher iv crypt = B.concat $ douncbc cipher iv $ block blocklen crypt
+	where
+		blocklen = B.length iv
+
+douncbc :: (Block -> Block) -> IV -> [Block] -> [Block]
+douncbc _ _ [] = []
+douncbc cipher iv (x:xs) = plain : douncbc cipher x xs
+	where
+		ivi = asInt iv
+		xdec = asInt $ cipher x
+		plain = asStr $ ivi `xor` xdec
 
 ecb :: (Block -> Block) -> IV -> B.ByteString -> B.ByteString
 ecb cipher iv plain = B.concat [cipher pblock | pblock <- pblocks]
